36ff6f719f8f0c798965a32f747da81c4dad008e
xokhotax <okhotai@gmail.com>
2022-04-03 12:18:08 +0300

     ProjectCreate

---
+++ WWTBAM/WWTBAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WWTBAM
+//
+//  Created by Ivan Okhota on 03.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
